77d13e612f4549d22e3002c188a89eca88a33456
Allan Amaral <allanmaralr@gmail.com>
2022-10-14 23:50:23 -0300

    Add User model

---
+++ GHFollowers/Models/User.swift
@@ -0,0 +1,22 @@
+//
+//  User.swift
+//  GHFollowers
+//
+//  Created by Allan Amaral on 14/10/22.
+//
+
+import Foundation
+
+struct User {
+    var login: String
+    var avatarURL: String
+    var name: String?
+    var location: String?
+    var bio: String?
+    var publicRepos: Int
+    var publicGists: Int
+    var htmlURL: String
+    var following: Int
+    var followers: Int
+    var createdAt: String
+}
