6754ffc88b2f86621d495c4677e2318ed2e072fc
Allan Amaral <allanmaralr@gmail.com>
2022-10-14 23:36:34 -0300

    refactor: reorganizer folder structure

---
+++ GHFollowers/Models/Follower.swift
@@ -0,0 +1,13 @@
+//
+//  Follower.swift
+//  GHFollowers
+//
+//  Created by Allan Amaral on 14/10/22.
+//
+
+import Foundation
+
+struct Follower {
+    var login: String
+    var avatarURL: String
+}
